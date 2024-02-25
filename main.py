9dd5bca8a013d7c7fdef2049bf71cc18d501cc8c
doquendo0 <132039477+doquendo0@users.noreply.github.com>
2024-02-25 11:38:36 -0800

    Dynamic Recognition
    Velocity adjustments may be necessary for different keyboards.
    
    pianississimo (very very quiet)
    pianissimo (very quiet)
    piano (quiet)
    mezzo-piano (moderately quiet)
    mezzo-forte (moderately loud)
    forte (loud)
    fortissimo (very loud)
    fortississimo (very very loud)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pygame import midi
 import keyboard
-import time
 
 midi.init()
 
@@ -21,20 +20,25 @@ try:
 
             for event in midi_events:
                 data, timestamp = event[0], event[1]
-                status, control, velocity, _ = data
+                status, note, velocity, _ = data
                 
-                if 176 <= status <= 191 and control == 64: # Check for control change and sustain pedal messages
-                    if velocity >= 64 and not pedal_state:  # pedal pressed
-                        pedal_state = True
-                        pressed_time = time.time()
-                        print("Sustain Pressed")
-                    elif velocity < 64 and pedal_state:  # pedal released
-                        pedal_state = False
-                        if pressed_time is not None:
-                            duration = time.time() - pressed_time
-                            print(f"Sustain Released. Duration: {duration:.2f} seconds")
-                            pressed_time = None
-
+                if status == 144 and velocity > 0: # key pressed
+                    if velocity <= 29:
+                        print(f"pianississimo {velocity}")
+                    elif 30 <= velocity <= 39:
+                        print(f"pianissimo {velocity}")
+                    elif 40 <= velocity <= 49:
+                        print(f"piano {velocity}")
+                    elif 50 <= velocity <= 59:
+                        print(f"mezzo-piano {velocity}")
+                    elif 60 <= velocity <= 69:
+                        print(f"mezzo-forte {velocity}")
+                    elif 70 <= velocity <= 79:
+                        print(f"forte {velocity}")
+                    elif 80 <= velocity <= 89:
+                        print(f"fortissimo {velocity}")
+                    elif 90 <= velocity:
+                        print(f"fortississimo {velocity}")
 finally:
     # Close the midi interface
     midi.quit()
