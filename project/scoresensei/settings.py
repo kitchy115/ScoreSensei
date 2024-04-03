0529faaea4396c1caa37ad87df712bf0cef34647
David-CSUSM <herna1334@csusm.edu>
2024-04-03 16:49:25 -0700

    add example homepage

--- project/scoresensei/settings.py
+++ project/scoresensei/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
